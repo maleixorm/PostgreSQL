d4881e42fe795200ff1bf87dc6b78cb8d053eb1a
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-08-02 18:22:32 -0300

    Consultas com Filtro no PGSQL

--- filtros.sql
+++ filtros.sql
@@ -23,3 +23,36 @@ SELECT *
   FROM aluno
  WHERE nome LIKE 'Di_go'
 ;
+
+SELECT * 
+  FROM aluno
+ WHERE cpf IS NULL
+;
+
+SELECT * 
+  FROM aluno
+ WHERE cpf IS NOT NULL
+;
+
+SELECT * 
+  FROM aluno
+ WHERE idade BETWEEN 10 AND 40
+;
+
+SELECT *
+  FROM aluno
+ WHERE nome LIKE 'D%'
+   AND cpf IS NOT NULL
+;
+
+SELECT *
+  FROM aluno
+ WHERE nome LIKE 'Diogo'
+    OR nome LIKE 'Rodrigo'
+;
+
+SELECT *
+  FROM aluno
+ WHERE nome LIKE 'Diogo'
+   AND nome LIKE 'Nico%'
+;
