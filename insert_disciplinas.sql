ee71487c0471089a2c1a79e41b5d6d31b1ab5451
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-11-19 16:34:21 -0300

    Insert, update e delete

--- insert_disciplinas.sql
+++ insert_disciplinas.sql
@@ -1,7 +1,13 @@
 insert into disciplinas 
 values 
-(1, 'português', 'literatura e gramática'),
-(2, 'matemática', 'algebra e geometria'),
-(3, 'física', 'cinemática e dinâmica');
+(4, 'inglês', 'verb to be e past tense'),
+(5, 'geografia', 'cartografia e geopolítica'),
+(6, 'ciências', 'anatomia e fisiologia'),
+(7, 'mecatrônica', 'robótica e programação');
 
 select * from disciplinas;
+
+update disciplinas set nome = 'biologia'
+where id_disciplina = 6;
+
+delete from disciplinas where id_disciplina = 7;
