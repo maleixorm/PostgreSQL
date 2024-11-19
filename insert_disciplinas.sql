46d99e6ebdd5b3f31872c056bc65589a34cd7797
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-11-19 15:01:51 -0300

    Inserindo registros na tabela disciplinas

---
+++ insert_disciplinas.sql
@@ -0,0 +1,7 @@
+insert into disciplinas 
+values 
+(1, 'português', 'literatura e gramática'),
+(2, 'matemática', 'algebra e geometria'),
+(3, 'física', 'cinemática e dinâmica');
+
+select * from disciplinas;
